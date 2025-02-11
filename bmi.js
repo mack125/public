6687a95fd174f7fe6214d320aca04ba64c7c2385
Mcinally-Michael <mcinally_michael@bah.com>
2025-02-11 15:38:53 -0500

    restarting

--- bmi.js
+++ bmi.js
@@ -2,13 +2,12 @@
 
 var bmi = 0
 
-function bmiCalc(height,weight) {
-    //let height = indexCopy.getElementById(height).value
-    //let weight = indexCopy.getElementById(weight).value
+function bmiCalc() {
+    let height = document.getElementById('height').value
+    let weight = document.getElementById('weight').value
     //bmiCalc.weight = prompt("what is your weight in kg?");
     //bmiCalc.height = prompt("what is your height in meters?");
     bmi = ((weight / (height * height)).toFixed(2));
     alert(" your bmi is " + bmi + "!");
     return bmi;
     };
-export {bmiCalc};
